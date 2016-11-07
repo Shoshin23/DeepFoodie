abee5bfce476fd5f3640cbdcd27c34ecb1a00453
Karthik Kannan <karthikk.314@gmail.com>
2016-11-07 13:34:03 +0530

    -Initial Commit. - Add the Clarifai pod, get it to recognise images successfully - TODO: Simple UI, Dispatch.async, etc.

--- DeepFoodie/ViewController.swift
+++ DeepFoodie/ViewController.swift
@@ -7,13 +7,71 @@
 //
 
 import UIKit
+import Clarifai
+import ImagePicker
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController,ImagePickerDelegate {
+
+    @IBAction func cameraButton(_ sender: UIButton) {
+        
+       // var app = ClarifaiApp.init(appID: "VyvexuzVef1qsSuW_ZQyE6iUW_H1DKiWXMieAruL", appSecret: "3W8Y3AHV-26n6hbyYeJv_6uqM2vAOkpiEnN9QeFW")
+        
+        let imagePickerController = ImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+
+        
+    }
+    
+    func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        
+    }
+    func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        var app = ClarifaiApp.init(appID: "VyvexuzVef1qsSuW_ZQyE6iUW_H1DKiWXMieAruL", appSecret: "3W8Y3AHV-26n6hbyYeJv_6uqM2vAOkpiEnN9QeFW")
+
+        app?.getModelByName("food-items-v1.0", completion: { (model, error) in
+            var clarifaiImg = ClarifaiImage.init(image: images.first)
+            
+            if error != nil {
+                print("some error here.")
+            }
+            else {
+                model?.predict(on: [clarifaiImg!], completion: { (output, error) in
+                    if error != nil {
+                        print("some error in the output \(error)")
+                        
+                    } else {
+                        var op = output?[0]
+                        print(op!)
+                        
+                        for concept in (op?.concepts)! {
+                            print(concept.conceptName)
+                            print(concept.score)
+                        }
+                        
+                    }
+                })
+            }
+        })
+        
+
+    }
+    func cancelButtonDidPress(_ imagePicker: ImagePickerController) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
